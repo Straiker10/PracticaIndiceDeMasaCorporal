da5a81132547beb53b0e10d43efc74e46f16c874
L.quintal <WichOo.b.boy@gmail.com>
2017-03-30 00:40:48 -0600

    Creacion de las variables
    Se insertarion las variables peso y altura que se utilizaran el la
    aplicación.

--- IndiceMasaCorporal/app/src/main/java/com/example/diosemirnah/indicemasacorporal/MainActivity.java
+++ IndiceMasaCorporal/app/src/main/java/com/example/diosemirnah/indicemasacorporal/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.diosemirnah.indicemasacorporal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText peso, altura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
